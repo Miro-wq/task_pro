f114011e843413c411cb60a9b64b977a263ea77a
Miro <117939922+Miro-wq@users.noreply.github.com>
2025-03-01 21:09:17 +0200

    Sidebar.js & CSS

--- src/components/SideBar/SideBar.js
+++ src/components/SideBar/SideBar.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styles from './SideBar.module.css';
+import cactus from '../../assets/images/cactus.png';
 
 function Sidebar() {
     return (
@@ -9,6 +10,38 @@ function Sidebar() {
                     <use xlinkHref="/assets/icons/symbol-defs.svg#icon-black_icon" />
                 </svg>
                 Task Pro</h1>
+
+            <div className={styles.myBoardsSection}>
+                <h2 className={styles.myBoardsTitle}>My boards</h2>
+                <hr className={styles.divider} />
+
+                <div className={styles.createBoardContainer}>
+                    <span className={styles.createBoardText}>Create a new board</span>
+                    <button className={styles.createBoardButton}>
+                        <span className={styles.plusSign}>+</span>
+                    </button>
+
+                </div>
+                <hr className={styles.divider} />
+
+            </div>
+
+            <div className={styles.block}>
+                <div className={styles.blockContent}>
+                    <img src={cactus} alt="Cactus" className={styles.cactusImage} />
+                    <div className={styles.blockText}>
+                        <p className={styles.blockDescription}>If you need help with <span className={styles.task}>TaskPro</span>, check out our support resources or reach out to our customer support team.</p>
+                    </div>
+                    <div className={styles.blockButtonContainer}>
+                        <div className={styles.helpContainer}>
+
+                            {/* trebuie vazut daca e buton cu link sau lin k simplu */}
+                            <div className={styles.helpIcon}>?</div>
+                            <span className={styles.helpText}>Need help?</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
